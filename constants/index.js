b0b4f6fb1b7dfa57eb7905e3c9fcfd57ae9b9265
Edison Decena <decaedison24@gmail.com>
2023-09-25 18:45:46 +0800

    first commit

---
+++ constants/index.js
@@ -0,0 +1,75 @@
+///categories
+
+export const categories = [
+  {
+    id: 1,
+    title: "Fruit Juice",
+  },
+  {
+    id: 2,
+    title: "Vegies Juice",
+  },
+  {
+    id: 3,
+    title: "Dessert",
+  },
+  {
+    id: 4,
+    title: "Milktea",
+  },
+  {
+    id: 5,
+    title: "Frappe",
+  },
+]
+
+export const coffeeItems = [
+  {
+    id: 1,
+    name: 'Grape Juice',
+    price: '250',
+    volume: '116 ml',
+    stars: '4.6',
+    image: require('../assets/images/Grape.jpg'),
+    desc: 'Grapes are packed with antioxidants, such as resveratrol, flavonoids, and quercetin, which help protect your cells from damage caused by free radicals. These antioxidants have been linked to a reduced risk of chronic diseases, including heart disease and cancer.'
+  },
+  
+  {
+    id: 2,
+    name: 'Orange Juice',
+    price: '250',
+    volume: '110 ml',
+    stars: '4.3',
+    image: require('../assets/images/Orange.jpg'),
+    desc: ' Oranges are one of the best sources of vitamin C, which is essential for a strong immune system, wound healing, and collagen production. Vitamin C also acts as a powerful antioxidant, protecting cells from damage caused by free radicals.'
+  },
+  
+  {
+    id: 3,
+    name: 'Strawberry Juice',
+    price: '200',
+    volume: '100 ml',
+    stars: '4.0',
+    image: require('../assets/images/Strawerry.jpg'),
+    desc: ' Strawberries contain essential minerals like calcium, magnesium, and potassium, which support bone health and reduce the risk of osteoporosis.'  },
+  
+  {
+    id: 4,
+    name: 'Mango Juice',
+    price: '100',
+    volume: '80 ml',
+    stars: '3.5',
+    image: require('../assets/images/Mango.jpg'),
+    desc: 'Mango juice is a rich source of essential vitamins, particularly vitamin C and vitamin A. Vitamin C boosts the immune system, aids in wound healing, and supports healthy skin, while vitamin A is crucial for vision, immune function, and skin health.'  },
+  
+  {
+    id: 5,
+    name: 'Green Apple',
+    price: '150',
+    volume: '120 ml',
+    stars: '4.7',
+    image: require('../assets/images/GreenJuice.png'),
+    desc: 'Green apples are packed with antioxidants, including vitamin C, which help combat oxidative stress and reduce the risk of chronic diseases by neutralizing harmful free radicals.'  },
+  
+
+]
---
+++ theme/index.js
@@ -0,0 +1,9 @@
+export const themeColors = {
+   // bgColor: opacity=> `rgba(145, 73, 0, ${opacity})`,
+   // bgColor: opacity=> `rgba(65, 2, 0, ${opacity})`,
+   bgColor: opacity=> `rgba(255, 255, 0, 0.5), ${opacity})`,
+   bgLight: 'rgba(255, 255, 0, 0.5)',
+   bgDark: 'rgba(255, 255, 0, 0.5)',
+   text: 'rgba(0, 0, 0, 0.5)'
+   
+}   
